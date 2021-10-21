7edd27e0662b3cb5933f63e1bea42f54d8b8ead0
Fred Lahode <lahode@infomaniak.ch>
2021-10-21 10:25:09 +0200

    Add authentication API with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FilmController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\AuthenticationController;
 
-Route::resource('films', FilmController::class);
-Route::resource('categories', CategoryController::class);
+// Register new user
+Route::post('/create-account', [AuthenticationController::class, 'createAccount']);
+
+// Login user
+Route::post('/signin', [AuthenticationController::class, 'signin']);
+
+// Add sanctum middleware to protect our routes.
+Route::middleware('auth:sanctum')->group(function () {
+  Route::resource('films', FilmController::class);
+  Route::resource('categories', CategoryController::class);
+  Route::post('/sign-out', [AuthenticationController::class, 'logout']);
+});
+
+// Add login route because Laravel needs it (Or add Accept: application/json to all requests),
+Route::get('/login', function () {
+  return response()->json([
+    'status_code' => 400,
+    'message' => 'error'
+  ]);
+})->name('login');
